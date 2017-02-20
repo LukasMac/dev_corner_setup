4ce89b7a8de0790afec80406efb8d6728cf68851
Lukas Maciulis <lukas.maciulis@gmail.com>
2017-02-20 13:43:03 +0100

    Add logger to express server

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,12 +1,15 @@
-const port = 3001
-var express = require('express')
-var app = express()
+const port = 3001;
+var express = require('express');
+var morgan = require('morgan')
+var app = express();
+
+app.use(morgan('dev'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, function () {
-  console.log('Example app listening on port ' + port)
-})
+  console.log('App listening on port ' + port);
+});
 
